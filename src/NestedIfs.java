f2fe23684f1a56e98331f594f5ca4243d568d039
Aaron Haight <ahaight1013@gmail.com>
2016-07-18 22:27:57 -0400

    finished nestedifs lesson

--- src/NestedIfs.java
+++ src/NestedIfs.java
@@ -2,5 +2,18 @@
  * Created by aaronhaight on 7/16/16.
  */
 public class NestedIfs {
-    
+    public static void main(String[] args) {
+        float students = 0.0f;
+        float rooms = 4.0f;
+
+        if (students > 0.0f) {
+            if (rooms > 0.0f)
+                System.out.println(students / rooms);
+        }
+        else
+            System.out.println("No students");
+
+        System.out.println();
+        System.out.println("** end program **");
+    }
 }
