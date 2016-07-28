3fe800a1281d6241b8031f0c03bd8f1b4f332b19
Aaron Haight <ahaight1013@gmail.com>
2016-07-27 21:39:26 -0400

    added arrays and for each loop classes

---
+++ src/Arrays.java
@@ -0,0 +1,23 @@
+/**
+ * Created by aaronhaight on 7/27/16.
+ */
+public class Arrays {
+    public static void main(String[] args) {
+
+//      long way of doing it...
+//      float[] theVals = new float[3];
+//      theVals[0] = 10.0f;
+//      theVals[1] = 20.0f;
+//      theVals[2] = 15.0f;
+
+//      better way of doing it...
+        float[] theVals = { 10.0f, 20.0f, 15.0f };
+
+        float sum = 0.0f;
+
+        for(int i = 0; i < theVals.length; i++)
+            sum += theVals[i];
+
+        System.out.println(sum);
+    }
+}
---
+++ src/ForEachLoop.java
@@ -0,0 +1,15 @@
+/**
+ * Created by aaronhaight on 7/27/16.
+ */
+public class ForEachLoop {
+    public static void main(String[] args) {
+        float[] theVals = { 10.0f, 20.0f, 15.0f };
+
+        float sum = 0.0f;
+
+        for(float currentVal : theVals)
+            sum += currentVal;
+
+        System.out.println(sum);
+    }
+}
